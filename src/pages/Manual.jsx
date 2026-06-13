3d1246c699346a4671d8de503a3876db2440b6c4
GradPath <jyw8024@gmail.com>
2026-06-13 19:55:04 +0900

    fix: 설명서 스크롤 유지하면서 스크롤바만 숨김

--- src/pages/Manual.jsx
+++ src/pages/Manual.jsx
@@ -1,12 +1,15 @@
 export default function Manual() {
   return (
-    <div className="-mx-6 -my-8 overflow-hidden">
+    <div className="-mx-6 -my-8" style={{ overflow: 'hidden' }}>
       <iframe
         src="/manual.html"
-        className="w-full border-0"
-        style={{ height: 'calc(100vh - 48px)', display: 'block' }}
+        style={{
+          height: 'calc(100vh - 48px)',
+          width: 'calc(100% + 17px)',
+          display: 'block',
+          border: 'none',
+        }}
         title="GradPath 사용 설명서"
-        scrolling="no"
       />
     </div>
   )
