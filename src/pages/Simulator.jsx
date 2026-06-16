859fd70bbae4d81f3aa7a5582368db8fa4a6936d
GradPath <jyw8024@gmail.com>
2026-06-16 11:07:01 +0900

    fix: Simulator useEffect import 누락(크래시 수정), manual.html 과목 수/게시글 제한 수정

--- src/pages/Simulator.jsx
+++ src/pages/Simulator.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useMemo, useRef } from 'react'
+import { useState, useCallback, useMemo, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useSimulator } from '@/hooks/useSimulator'
 import Button from '@/components/Button'
